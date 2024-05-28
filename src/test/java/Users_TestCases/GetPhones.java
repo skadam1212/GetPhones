3090fb912bfb22b12f00f38f714afc74dd463e83
Sandeep.Kadam <sandeep.kadam@coforge.com>
2024-05-28 15:36:22 +0530

    Few Changes added

--- src/test/java/Users_TestCases/GetPhones.java
+++ src/test/java/Users_TestCases/GetPhones.java
@@ -20,47 +20,37 @@ import org.json.JSONObject;
 
 
 public class GetPhones {
-	
-	@Test
-		void getPhones() {
-		
-		
-		
-		Response res= given()
-						.contentType("ContentType.JSON")
-				
-						.when()	
-							.get("https://api.restful-api.dev/objects");
-		
-		JsonPath jsonpath=res.jsonPath();
-		String id=jsonpath.getString("id");
-		System.out.println(id);
-		Assert.assertNotNull(id);
-		
+
+    @Test
+    void getPhones() {
+
+        Response res = given()
+                .contentType("ContentType.JSON")
+
+                .when()
+                .get("https://api.restful-api.dev/objects");
+
+        JsonPath jsonpath = res.jsonPath();
+        String id = jsonpath.getString("id");
+        System.out.println(id);
+        Assert.assertNotNull(id);
+
 //		String price=res.jsonPath().get("data[3].price").toString();
 //		System.out.println(price);
-		
-		
-		JSONArray jsonArray=new JSONArray(res.asString());
-		
-		for (int i=0;i<jsonArray.length();i++)
-		{
-			JSONObject jsonObject1=jsonArray.getJSONObject(i);
+        JSONArray jsonArray = new JSONArray(res.asString());
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject1 = jsonArray.getJSONObject(i);
 //			
 //			String price1= jsonObject1.get("data").toString();
 //			System.out.println(price1);
-			
-			String value1= jsonObject1.getString("name");	
-			String value2="Apple";
-//			String price1= jsonObject1.getJSONObject("data").getString("price");	
-			
-			if (value1.contains(value2)) {
-				System.out.println(value1);
-			}
-			
-//			String price1= jsonObject1.get("data").toString();
-//			System.out.println(price1);
-		}
-		
-	}
+
+            String value1 = jsonObject1.getString("name");
+            String value2 = "Apple";
+
+            if (value1.contains(value2)) {
+                System.out.println(value1);
+            }
+        }
+    }
 }
